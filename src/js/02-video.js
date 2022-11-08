a3529d6cb8fd285d49b4671b8bb32b4fcc24b330
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-11-08 17:50:20 +0200

    Все задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const frame = document.querySelector('#vimeo-player');
+const player = new Player(frame);
+const KEY = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(KEY);
+if (!currentTime) {
+  currentTime = 0;
+}
+
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(KEY, data.seconds);
+}
